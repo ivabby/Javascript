687cad34053cf0ae4de4fdc69dca9d76839eec75
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2023-10-15 15:24:01 +0530

    Comparsions in JS

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // True as null is converted to a number > 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === -> Checks datatype along with value.
+
+console.log("2" === 2); // false
