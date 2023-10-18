d678c006f93f96c51cd5c6a8ad1f3bf9e76f8619
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2023-10-18 10:25:48 +0530

    Numbers and Math in JS

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+const score = 400 // Number
+console.log(score);
+
+// Object number has some additional features
+const balance = new Number(100) // Object
+// console.log(balance);
+
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3)); // Round off to number 
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // Print US Standard value
+// console.log(hundreds.toLocaleString('en-IN')); // Print Indian Standard value
+
+// console.log(Number.MAX_VALUE); // MAX_VALUE
+// console.log(Number.MIN_VALUE); // MIN_VALUE
+
+
+/**
+ * Maths Function
+ */
+
+console.log(Math);
+
+/**
+ * Math.floor -> Round to above value
+ * Math.ceil -> Round to below value
+ * Math.random -> Get a random number between 0 and 1
+ */
